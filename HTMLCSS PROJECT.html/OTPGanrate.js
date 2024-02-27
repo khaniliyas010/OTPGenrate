ab8e209f642f478b5fd016c35e0cc1f255a6ccfb
iliyaskhan <iliyas.k2003@gmail.com>
2024-02-27 23:23:18 +0530

    init

---
+++ HTMLCSS PROJECT.html/OTPGanrate.js
@@ -0,0 +1,13 @@
+// Function to generate OTP
+function generateOTP() {
+
+	let digits = '0123456789';
+	let OTP = '';
+	for (let i = 0; i < 4; i++) {
+		OTP += digits[Math.floor(Math.random() * 10)];
+	}
+	return OTP;
+}
+
+console.log("OTP of 4 digits: ")
+console.log(generateOTP());
